8351cbeed1e4a10e049f65458b36fbb519052718
Angelina <angelshakirova@gmail.com>
2020-10-23 00:10:44 +0300

    first styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ItemList from '../ItemList/ItemList';
 import InputItem from '../InputItems/InputItems';
 import Footer from '../Footer/Footer';
+import './App.css';
 
 const count = 6;
 
@@ -17,10 +18,10 @@ const App = () => {
       value: 'Навести порядок'
     }];
   return (
-    <div>
-      <h1>todos</h1>
-      <InputItem/>
-      <ItemList todoItems={todoItems}/>
+    <div className="wrap">
+      <h1 className="wrap__title">todos</h1>
+      <InputItem className="wrap__input" />
+      <ItemList todoItems={todoItems} className="wrap__item"/>
       <Footer count={count}/>
     </div>
   )
