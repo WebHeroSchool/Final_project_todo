ba4302a7588cf8b4a65734c6b72884b49721aa65
Angelina <angelshakirova@gmail.com>
2020-10-27 14:01:15 +0300

    first stylization

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import Button from '@material-ui/core/Button';
+import classnames from 'classnames';
 
-const Footer = ({ count }) => (
-  <div>Осталось сделать {count} дел</div>
-);
+const Footer = ({ items }) => {
+  let countArr = items.filter(item => !item.isDone)
+  return (
+    <div className={styles.wrap}>
+      <div className={styles.count}>Оставшиеся дела: {countArr.length}</div>
+      <div>
+        <Button style={{fontSize: '10px'}}>Все</Button>
+        <Button style={{fontSize: '10px'}}>Активные</Button>
+        <Button style={{fontSize: '10px'}}>Выполненные</Button>
+      </div>
+      <Button style={{fontSize: '10px'}}>Удалить выполненные</Button>
+    </div>
+  )
+};
 
 export default Footer;
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import classnames from 'classnames';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 import styles from './Item.module.css';
 
-const Item = ({ value, isDone }) => (
-  <li className = {
-    classnames({
-      [styles.item]: true,
-      [styles.done]: isDone
-    })
-  }>{value}</li>
-);
+const Item = ({ value, isDone }) => {
+  const [show, setShow] = React.useState(false);
+
+  const mouseLeave = () => {
+    setShow(false);
+  };
+
+  const mouseEnter = () => {
+    setShow(true);
+  };
+
+  return (
+  <div className = {styles.wrap} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+    <li className = {
+      classnames({
+        [styles.item]: true,
+        [styles.done]: isDone
+      })
+    }>
+      {value}
+      </li>
+    <IconButton
+      aria-label="delete"
+      className={!show ? styles.delete : ''}>
+      <DeleteIcon/>
+    </IconButton>
+  </div>);
+};
 
 export default Item;
