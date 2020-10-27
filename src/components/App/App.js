79281eb360e857d2cf2028edaa70ae4a60c569db
Angelina <angelshakirova@gmail.com>
2020-10-27 16:40:52 +0300

    state of App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,8 +5,8 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-  render() {
-    const todoItems = [
+  state = {
+    todoItems: [
       {
         value: 'Закончить модуль',
         isDone: true
@@ -18,13 +18,15 @@ class App extends React.Component {
       {
         value: 'Навести порядок',
         isDone: true
-      }];
+      }]
+  };
+  render() {
     return (
       <div className={styles.wrap}>
         <h1 className={styles.title}>Список дел:</h1>
-        <InputItem />
-        <ItemList todoItems={todoItems} />
-        <Footer items={todoItems}/>
+        <InputItem/>
+        <ItemList todoItems={this.state.todoItems}/>
+        <Footer items={this.state.todoItems}/>
       </div>
     )
   }
