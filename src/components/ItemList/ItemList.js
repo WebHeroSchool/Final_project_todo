1a26dfcad6b71233704d76f5cec042cecfb57501
Angelina <angelshakirova@gmail.com>
2020-10-29 23:26:45 +0300

    checked item

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,7 +3,14 @@ import Item from '../Item/Item';
 
 const ItemList = ({ todoItems, onMouseClick }) => (
   <ul>
-    {todoItems.map(item => <Item value = {item.value} isDone = {item.isDone} key = {item.value} onMouseClick = {onMouseClick}/>)}
+    {todoItems.map(
+      item => <Item
+      value = {item.value}
+      isDone = {item.isDone}
+      key = {item.value}
+      onMouseClick = {onMouseClick}
+      id = {item.id}
+      />)}
   </ul>
 );
 
